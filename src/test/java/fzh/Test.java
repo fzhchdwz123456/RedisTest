08d94c466b8ebb937b65e90527af60302405b27a
fengzihao <447284528@qq.com>
2017-12-15 21:34:10 +0800

    first commit

---
+++ src/test/java/fzh/Test.java
@@ -0,0 +1,16 @@
+package fzh;
+
+import redis.clients.jedis.Jedis;
+
+public class Test {
+
+	@org.junit.Test
+	public void testRedis()throws Exception {
+		Jedis jedis=new Jedis("120.24.90.162",6379);
+		jedis.set("fzh", "Äã×îÅ£1212121");
+		String string = jedis.get("fzh");
+		System.out.println("String ="+string);
+		jedis.close();
+	}
+	
+}
